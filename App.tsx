6b8514c0fb4e9d461abad76587326a3448006655
biruk-kassaw <se.biruk.kassaw@gmail.com>
2022-07-15 12:43:55 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './style.css';
+import Nav from './components/nav';
+
+console.log(Nav);
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
