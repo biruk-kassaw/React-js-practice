fc499dfd9358c4b7a0e4f52634b8307fb4fc1d47
biruk-kassaw <se.biruk.kassaw@gmail.com>
2022-07-15 16:35:30 +0300

    state management and event handling

--- components/counter.jsx
+++ components/counter.jsx
@@ -2,18 +2,51 @@ import React from 'react';
 
 class Counter extends React.Component {
   state = {
-    count: 0,
+    count: 1,
+    chores: ['study', 'eat', 'exercise'],
   };
-  countIncreaser() {
-    this.state.count += 1;
+  countIncreaser = (num) => {
+    this.setState({ count: this.state.count + 1 });
+    console.log(num);
+  };
+  countDecreaser = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  hcolor() {
+    if (this.state.count > 0) {
+      return 'bg-black';
+    } else {
+      return 'bg-primary';
+    }
+  }
+  numberOfChores() {
+    if (this.state.chores.length < 1) {
+      return <p>no chore</p>;
+    } else {
+      return (
+        <ul>
+          {this.state.chores.map((chor) => {
+            return <li key={chor}>{chor}</li>;
+          })}
+        </ul>
+      );
+    }
   }
   render() {
     return (
       <div>
-        <h1 className=""> number of counts {this.state.count}</h1>
-        <button onClick={this.countIncreaser} className="btn btn-primary">
+        <h1 className={this.hcolor()}> number of counts {this.state.count}</h1>
+        <button
+          onClick={() => this.countIncreaser(1)}
+          className="btn btn-primary"
+        >
           increase count
         </button>
+
+        <button onClick={this.countDecreaser} className="btn btn-primary m-5">
+          decrease count
+        </button>
+        {this.numberOfChores()}
       </div>
     );
   }
