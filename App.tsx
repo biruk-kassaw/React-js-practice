6aabf8a722f5d8e756d72ff749d9d2e5299489f4
biruk-kassaw <se.biruk.kassaw@gmail.com>
2022-07-17 18:09:43 +0300

    delete functionality

--- App.tsx
+++ App.tsx
@@ -2,19 +2,32 @@ import * as React from 'react';
 import './style.css';
 import Nav from './components/nav';
 import Counter from './components/counter';
-import Table from "./components/movieTable"
+import Table from './components/movieTable';
 console.log(Nav);
 console.log(Counter);
 
-export default function App() {
-  return (
-    <div>
-      <Nav />
-      <Table />
-      <Counter />
-
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    counts: [
+      { id: 1, value: 1 },
+      { id: 2, value: 1 },
+      { id: 3, value: 1 },
+      { id: 4, value: 1 },
+      { id: 5, value: 1 },
+    ],
+  };
+  handleDelete = (id) => {
+    const counts = this.state.counts.filter((count) => count.id !== id);
+    this.setState({ counts });
+  };
+  render() {
+    return (
+      <div>
+        <Nav counts={this.state.counts.length} />
+        <button className="btn btn-secondary m-2">Reset</button>
+        {/* <Table /> */}
+        <Counter counts={this.state.counts} handleDelete={this.handleDelete} />
+      </div>
+    );
+  }
 }
