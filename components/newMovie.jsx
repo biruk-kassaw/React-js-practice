115f42e0325d2cc7f9c273a7cf36f3d1ba91d9e7
biruk-kassaw <se.biruk.kassaw@gmail.com>
2022-08-15 16:44:43 +0300

    validation the form on submission

--- components/newMovie.jsx
+++ components/newMovie.jsx
@@ -5,6 +5,58 @@ class NewMovie extends React.Component {
     newMovie: { title: '', genre: '', numberInStock: '', rate: '' },
     errors: {},
   };
+
+  validateInput = (e)=>{
+    if(e.target.name =="numberInStock"){
+      if (parseInt(e.target.value) < 0) {
+        return'number in stock can not be less than 0';
+      }
+      if (parseInt(e.target.value) > 100) {
+        return'number in stock can not be greater than 100';
+      }
+    }
+  }
+
+  handleChange = (e) => {
+    let errors = {...this.state.errors}
+
+    let newMovie = { ...this.state.newMovie };
+    newMovie[e.target.name] = e.target.value;
+    let error = this.validateInput(e)
+    errors[e.target.name] = error
+
+    this.setState({ newMovie, errors });
+  };
+
+  validate = () => {
+    let errors = {};
+    const { numberInStock, rate } = this.state.newMovie;
+
+    if (parseInt(numberInStock) < 0) {
+      errors.numberInStock = 'number in stock can not be less than 0';
+    }
+    if (parseInt(numberInStock) > 100) {
+      errors.numberInStock = 'number in stock can not be greater than 100';
+    }
+    if (parseInt(rate) > 10) {
+      errors.numberInStock = 'rating can not be greater than 10';
+    }
+
+    return errors;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.newMovie);
+    const errors = this.validate();
+
+    this.setState({ errors });
+
+    if (Object.keys(errors).length == 0) {
+      console.log('submitted');
+    }
+  };
+
   render() {
     const { newMovie, errors } = this.state;
     return (
@@ -21,7 +73,7 @@ class NewMovie extends React.Component {
         <Input
           value={newMovie.numberInStock}
           name="numberInStock"
-          type="text"
+          type="number"
           onChange={this.handleChange}
           label="Enter Numbers In Stock"
           error={errors.numberInStock}
@@ -31,23 +83,32 @@ class NewMovie extends React.Component {
           <select
             className="custom-select mr-sm-2 form-control"
             id="inlineFormCustomSelect"
+            value={newMovie.genre}
+            onChange={this.handleChange}
+            name="genre"
           >
             <option value="action" selected>
               Action
             </option>
             <option value="comedy">Comedy</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+            <option value="thriller">Thriller</option>
           </select>
         </div>
         <Input
           value={newMovie.rate}
           name="rate"
-          type="text"
+          type="number"
           onChange={this.handleChange}
           label="Enter Rate"
           error={errors.rate}
         />
+        <button
+          disabled={Object.keys(this.validate()).length !== 0}
+          type="submit"
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
       </form>
     );
   }
